87913db93864f21b5a3854e52dc2ca9a8c14fabb
IAbdulSubhan <subhankhalidofficial@gmail.com>
2024-10-14 22:19:36 +0500

    Add funtionality to edit or Update product done change in controller, view, routes works on id base

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,12 +14,29 @@ class ProductsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
-
-    def show 
+  def show 
       @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product, notic: 'Product was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
+
+  
+
+
 
 
 
