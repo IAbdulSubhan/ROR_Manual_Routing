e8f932aff2f32cefed6cf366ace1fbfa8cbcb46b
IAbdulSubhan <subhankhalidofficial@gmail.com>
2024-10-14 22:02:25 +0500

    abi tak create new product and show list of created product on index page ka kam pura kiya hai

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+    def show 
+      @product = Product.find(params[:id])
+    end
+  end
+
+
+
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+end
